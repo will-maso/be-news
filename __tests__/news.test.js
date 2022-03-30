e7bcdaac5a14955d3eec746d41a738b3e441bbcf
will-maso <ww=m4705211@gmail.com>
2022-03-30 12:00:48 +0100

    done finally

--- __tests__/news.test.js
+++ __tests__/news.test.js
@@ -3,6 +3,7 @@ const testData = require("../db/data/test-data");
 const db = require("../db/connection");
 const app = require("../app");
 const request = require("supertest");
+const data = require("../endpoints.js");
 
 afterAll(() => db.end());
 beforeEach(() => seed(testData));
@@ -362,97 +363,7 @@ describe.only("GET/api", () => {
       .get("/api")
       .expect(200)
       .then((result) => {
-        expect(result.body).toMatchObject({
-          "GET /api": {
-            description:
-              "serves up a json representation of all the available endpoints of the api",
-          },
-          "GET /api/topics": {
-            description: "serves an array of all topics",
-            queries: [],
-            exampleResponse: {
-              topics: [{ slug: "football", description: "Footie!" }],
-            },
-          },
-          "GET /api/articles": {
-            description: "serves an array of all topics",
-            queries: ["author", "topic", "sort_by", "order"],
-            exampleResponse: {
-              articles: [
-                {
-                  title: "Seafood substitutions are increasing",
-                  topic: "cooking",
-                  author: "weegembump",
-                  body: "Text from the article..",
-                  created_at: 1527695953341,
-                },
-              ],
-            },
-          },
-          "DELETE/api/comments/:comment_id": {
-            description: "returns 204 no content after successful deletion",
-            exampleResponse: { "err.status": 204 },
-          },
-          "GET/api/articles/:article_id": {
-            description: "returns article object with correct properties",
-            exampleResponse: {
-              comment_id: 1,
-              votes: 6,
-              created_at: "2020-10-11",
-              author: "william",
-              body: "this is my article",
-            },
-          },
-          "GET/api/users": {
-            description: "returns an array of all users",
-            exampleResponse: [
-              {
-                username: "william",
-              },
-            ],
-          },
-          "GET/api/articles/:article_id/comments": {
-            description:
-              "returns an array of all comments for a given article id",
-            exampleResponse: [
-              {
-                comment_id: 1,
-                votes: 5,
-                created_at: "2021-10-9",
-                author: "william",
-                body: "this is my comment",
-              },
-            ],
-          },
-          "POST/api/articles/:article_id/comments": {
-            description:
-              "responds with the posted comment when given correct input",
-            exampleResponse: {
-              comment_id: 1,
-              votes: 0,
-              created_at: "2046-12-07",
-              author: "william",
-              body: "this is my comment",
-            },
-            exampleBody: {
-              username: "william",
-              body: "this is my comment",
-            },
-          },
-          "PATCH/api/articles/:article_id": {
-            description: "returns the updated object when given correct input",
-            exampleResponse: {
-              author: "william",
-              title: "the saga",
-              article_id: 1,
-              body: "this is my article",
-              topic: "cats",
-              created_at: "2022-05-03",
-              votes: 100,
-            },
-            exampleBody: { inc_votes: 1 },
-          },
-        });
+        expect(result.body).toEqual(data);
       });
   });
 });

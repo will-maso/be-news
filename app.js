6c9dcfe8f8d0c86d666421af20761591cb9ecc7d
will-maso <ww=m4705211@gmail.com>
2022-03-31 10:51:17 +0100

    heroku

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
-
 const app = express();
 app.use(express.json());
+
 const { getTopics } = require("./controllers/topics.controllers");
 const {
   getArticleById,

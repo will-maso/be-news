95541580d981b74eef8e13341b01375591ff9855
will-maso <ww=m4705211@gmail.com>
2022-04-19 11:31:52 +0100

    heroku

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const cors = require("cors");
-app.use(cors());
 const express = require("express");
+const cors = require("cors");
 const apiRouter = require("./routes/api.router");
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 
